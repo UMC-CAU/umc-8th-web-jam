49fe647f569ab5596069722cb3a2fac4fb390519
jam0928 <jack020928@naver.com>
2025-03-28 19:27:55 +0900

    props drilling 예제 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import './App.css'
 import { useState } from 'react';
+import ButtonGroup from './components/ButtonGroup';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const handleIncreaseNumber = () => {
-    setCount(prev => prev + 1);
-  }
-  const handleDecreaseNumber = () => {
-    setCount(prev => prev - 1);
-  }
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
   return (
-     <>
+    <>
       <h1>{count}</h1>
-      <button onClick={handleIncreaseNumber}>숫자 증가</button>
-      <button onClick={handleDecreaseNumber}>숫자 감소</button>
-     </>
-  )
+      <ButtonGroup
+        handleIncrement={handleIncrement}
+        handleDecrement={handleDecrement}
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
