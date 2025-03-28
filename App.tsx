25e515aee9fdf6704d54f66ffebabf615e123b79
jam0928 <jack020928@naver.com>
2025-03-28 15:03:46 +0900

    2주차 실습

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+    return (
+        <>
+            <strong className='university'>중앙대학교</strong>
+            <p>잼/권재민</p>
+        </>
+    )
+}
+
+export default App
