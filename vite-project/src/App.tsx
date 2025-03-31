993092e63925489b3dc8645f5631ca7b65ffde2d
jam0928 <jack020928@naver.com>
2025-03-31 13:01:04 +0900

    TailwindCSS 적용 및 테스트

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.css';
 
 function App() {
-
   return (
-    <>
-     <h1>Hello React</h1>
-    </>
-  )
+    <div className="flex items-center justify-center min-h-screen bg-blue-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind CSS 적용 완료 🎉
+      </h1>
+    </div>
+  );
 }
 
-export default App
+export default App;
