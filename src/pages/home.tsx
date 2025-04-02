1d49f1f20789ae095252903f9048b846e1aaa664
jam0928 <jack020928@naver.com>
2025-04-02 16:43:19 +0900

    Homepage 추가

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,6 +1,17 @@
-// home.tsx
 const HomePage = () => {
-  return <h1>이곳은 홈페이지입니다.</h1>;
+  return (
+    <div className="relative w-full h-screen overflow-hidden">
+      <div
+        className="absolute inset-0 bg-cover bg-center blur-sm brightness-75 -z-10"
+        style={{ backgroundImage: `url('/wonder.png')` }}
+      />
+      <div className="flex justify-center items-center h-full">
+        <h1 className="text-white text-6xl md:text-8xl font-extrabold drop-shadow-lg hover:scale-450 transition duration-1500 ease-out">
+          JMDB
+        </h1>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
