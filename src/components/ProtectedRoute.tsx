d33984de1360f4492b42fcaded8e632a342e9178
jam0928 <jack020928@naver.com>
2025-05-01 18:46:54 +0900

    오타 수정 및 설명 주석 추가

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -2,18 +2,19 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 
-interface Props {
-  children: React.ReactNode;
+interface Props { // ProtectedRoute 컴포넌트가 받을 props의 타입을 정의
+  children: React.ReactNode; // 자식 요소를 감싸니까
 }
 
 const ProtectedRoute: React.FC<Props> = ({ children }) => {
-  const { isLoggedIn } = useAuth();
+  const { isLoggedIn } = useAuth(); // useAuth()로 
 
+  // 로그인 되지 않은 경우 로그인 창으로 리다이렉트
   if (!isLoggedIn) {
     return <Navigate to="/log-in" replace />;
   }
 
-  return <>{children}</>;
+  return <>{children}</>; //children을 그대로 렌더링
 };
 
 export default ProtectedRoute;
