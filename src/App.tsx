bdb9a2f5dfff6825569562877cc84b1c66621cfd
jam0928 <jack020928@naver.com>
2025-05-07 19:12:39 +0900

    Tanstack Query 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
 
 import HomePage from './pages/home.tsx';
 import NotFound from './pages/not-found.tsx';
@@ -45,8 +47,15 @@ const router = createBrowserRouter([
   },
 ]);
 
+const queryClient = new QueryClient();
+
+
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  ) 
 }
 
 export default App;
