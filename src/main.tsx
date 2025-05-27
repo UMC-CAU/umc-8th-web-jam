ceeb7ef6bfc390cfae09115ab452fc963b297d91
jam0928 <jack020928@naver.com>
2025-05-27 23:27:04 +0900

    사소한 오류 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { AuthProvider } from './context/AuthContext';
-import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
     <AuthProvider>
